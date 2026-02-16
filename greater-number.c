fb2cbd5327792bc024e442b7cef3c884e004ea41
itishagupta22 <itishagupta22@gmail.com>
2026-02-16 11:31:24 +0530

    Add files via upload

---
+++ greater-number.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+ int main () 
+ {
+    int a,b,c;
+    printf("enter 3 numbers = ");
+    scanf ("%d %d %d",&a,&b,&c);
+if (a>b && a>c) {
+    printf ("a is greatest");
+}
+else if ( b>a && b > c ) {
+        printf(" The number b is the greatest");
+    }
+    else {
+        printf (" The number c is the greatest");
+    }
+    return 0;
+ }
